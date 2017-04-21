9f4a43abe9c96e530864e2904041796bf16bf97f
YamilYanez <yamilenriqueyanezmorales@hotmail.com>
2017-04-20 22:08:42 -0500

    Estilos
    Mas estilos generales y diseño de la pantalla de club

--- resources/views/club.blade.php
+++ resources/views/club.blade.php
@@ -5,8 +5,14 @@
 		club={{ $club->id }}
 		nombre-club="{{ $club->nombreClub }}">
 	</crear-publicacion-modal>
+	<div class="club-header one-edge-shadow">
+		<img src="{{URL::asset('images/header.jpg')}}">
+		<div class="club-title">
+			<p>{{$club->nombreClub}}</p>
+		</div>
+	</div>
 	<div class="row">
-		<div class="col-sm-3 hidden-xs">
+		<!-- <div class="col-sm-3 hidden-xs">
 			<div class="panel panel-default sidepanel pull-right">
 				<div class="panel-heading">
 					{{ $club->nombreClub }}
@@ -19,16 +25,19 @@
 					<a class="btn btn-primary" data-toggle="modal" data-target="#crearPublicacionModal"><span class = "glyphicon glyphicon-plus">&nbsp;</span>Compartir imagen</a>
 				</div>
 			</div>
-		</div>
-		<div class="col-sm-3 col-sm-push-6 hidden-xs">
+		</div> -->
+		<div class="col-sm-3 col-sm-push-9 hidden-xs">
+			<div class="sidepanel">
+				<a class="btn btn-primary" data-toggle="modal" data-target="#crearPublicacionModal"><span class = "glyphicon glyphicon-plus">&nbsp;</span>Compartir imagen</a>
+			</div>
 			<div class="list-group sidepanel">
 				<li class="list-group-item">Ultima actividad (comenantarios, likes)</li>
 			</div>
 		</div>
-		<div class="col-sm-6 col-sm-pull-3">
+		<div class="col-sm-9 col-sm-pull-3 container-diver shadow">
 			<div class="row">
 				@foreach ($publicaciones as $publicacion)
-				<div class="col-md-4 col-sm-6 col-xs-4">
+				<div class="col-md-3 col-sm-4 col-xs-3 one-edge-shadow">
 					<img src="{{ URL::asset($publicacion->contenidoMinRuta) }}" width="100%" height="auto">
 				</div>
 				@endforeach
